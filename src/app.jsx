0199f9f1167884db7e0dd14a6636b29f14c068e2
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-01-28 16:08:33 +0300

    added App component

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App/App";
 
-import 'react-select/dist/react-select.css';
+import "react-select/dist/react-select.css";
 
-render(<div>Place your application here</div>, document.getElementById('app-root'));
+render(<App />, document.getElementById("app-root"));
---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>123</span>
+      </div>
+    );
+  }
+}
+
+export default App;
